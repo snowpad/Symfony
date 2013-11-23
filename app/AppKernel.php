de4dac1cfcede72711f436cb04cf2f39c6f39238
Sonam Sherpa <sonamsnowpad@gmail.com>
2013-11-23 03:39:47 +0100

    The Blog Model

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Lidl\MyBundle\LidlMyBundle(),
             new Blogger\BlogBundle\BloggerBlogBundle(),
+	    new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
